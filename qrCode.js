ea92d621cbb819323bdbaf7671bcdf7688a94dd7
JiruGutema <jethior1@gmail.com>
2024-10-09 15:09:37 +0300

    header

--- qrCode.js
+++ qrCode.js
@@ -5,37 +5,43 @@ import bodyParser from "body-parser";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 
+// Set up the directory path
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
-const port = 3502;
+const port = 3501;
+const folderPath = `${__dirname}/folder`;
 
-// Serve static files from the folder directory
-app.use(express.static("./folder"));
+// Create a folder if it doesn't exist
+if (!fs.existsSync(folderPath)) {
+  fs.mkdirSync(folderPath);
+}
+
+// Middleware to serve static files
+app.use(express.static(folderPath));
+app.use(express.static("./"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Function to generate QR code
 function generateQrCode(req, res, next) {
   const userInput = req.body["userInput"];
-  console.log(userInput);
+
   if (!userInput) {
     return res.status(400).send("No input provided.");
   }
 
   const qrCode = qr.image(userInput, { type: "png" });
-  const qrCodePath = "./folder/qrGen.png";
+  const qrCodePath = `${folderPath}/qrGen.png`;
 
   // Pipe the QR code to a file
-  const writeStream = fs.createWriteStream(qrCodePath);
-  qrCode.pipe(writeStream);
-
-  writeStream.on("finish", () => {
-    next(); // Proceed to the next middleware
-  });
-
-  writeStream.on("error", (err) => {
-    console.error("Error generating QR code:", err);
-    res.status(500).send("Error generating QR code.");
-  });
+  qrCode
+    .pipe(fs.createWriteStream(qrCodePath))
+    .on("finish", () => {
+      next(); // Proceed to the next middleware
+    })
+    .on("error", (err) => {
+      console.error("Error generating QR code:", err);
+      res.status(500).send("Error generating QR code.");
+    });
 }
 
 // Serve the homepage
@@ -45,7 +51,8 @@ app.get("/", (req, res) => {
 
 // Handle form submission and send the result page
 app.post("/result", generateQrCode, (req, res) => {
-  res.sendFile(`${__dirname}/folder/result.html`);
+  // Serve the static result page
+  res.sendFile(`${folderPath}/result.html`);
 });
 
 // Start the server
